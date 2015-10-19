4e1b4303a2b7b6a95b1bf503b7a8ce4746a72cf8
Raphaël Barrois <raphael.barrois@polytechnique.org>
2015-10-20 00:23:28 +0200

    Fix preference view.

--- app/src/main/java/com/pawciobiel/fgpst/FgpstPreferenceActivity.java
+++ app/src/main/java/com/pawciobiel/fgpst/FgpstPreferenceActivity.java
@@ -2,9 +2,9 @@ package com.pawciobiel.fgpst;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Patterns;
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 import java.util.regex.Pattern;
 
-public class FgpstPreferenceActivity extends PreferenceActivity {
+public class FgpstPreferenceActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
     public static final String PREFERENCES = "com.pawciobiel.fgpst.preferences";
 
@@ -26,76 +26,45 @@ public class FgpstPreferenceActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        getFragmentManager().beginTransaction()
+            .replace(android.R.id.content, new FgpstPreferenceFragment())
+            .commit();
+    }
 
-        Preference pref;
+    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
 
-        pref = findPreference("URL");
-        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                String oldValue = preferences.getString("URL", "");
-                if (!PrefValidator.isUrlValid((String)newValue)) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.text_legend_url_invalid), Toast.LENGTH_LONG).show();
-                    return false;
-                } else if (FgpstService.isRunning
-                        && (newValue.toString() != oldValue)) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.toast_prefs_restart),
-                            Toast.LENGTH_LONG).show();
-                }
-                sendBroadcastPrefChanged();
-                return true;
+        if (key.equals("URL")) {
+            String newValue = preferences.getString("URL", "");
+            if (!PrefValidator.isUrlValid((String)newValue)) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.text_legend_url_invalid), Toast.LENGTH_LONG).show();
+            } else if (FgpstService.isRunning) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_prefs_restart),
+                        Toast.LENGTH_LONG).show();
             }
-        });
-
-        pref = findPreference("vehicle_id");
-        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                String oldValue = preferences.getString("vehicle_id", "");
-                if (!PrefValidator.isVehicleIDValid((String) newValue)) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.pref_vehicle_id_invalid),
-                            Toast.LENGTH_LONG).show();
-                    return false;
-                } else if (FgpstService.isRunning
-                        && (newValue.toString() != oldValue)) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.toast_prefs_restart),
-                            Toast.LENGTH_LONG).show();
-                }
-                sendBroadcastPrefChanged();
-                return true;
+            sendBroadcastPrefChanged();
+        } else if (key.equals("vehicle_id")) {
+            String newValue = preferences.getString("vehicle_id", "");
+            if (!PrefValidator.isVehicleIDValid((String) newValue)) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.pref_vehicle_id_invalid),
+                        Toast.LENGTH_LONG).show();
+            } else if (FgpstService.isRunning) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.toast_prefs_restart),
+                        Toast.LENGTH_LONG).show();
             }
-        });
-
-        pref = findPreference("pref_gps_updates");
-        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                int oldValue = Integer.parseInt(preferences.getString
-                        ("pref_gps_updates", "0"));
-                if (newValue == null
-                        || newValue.toString().length() == 0
-                        || !Pattern.matches("^\\d{1,5}$", newValue.toString())) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.invalid_number), Toast.LENGTH_SHORT).show();
-                    return false;
-                } else if (Integer.parseInt(newValue.toString()) < 2) { // user has been warned
-                    Toast.makeText(getApplicationContext(), getString(R.string.pref_gps_updates_too_low), Toast.LENGTH_SHORT).show();
-                    return false;
-                } else if (FgpstService.isRunning
-                        && Integer.parseInt(newValue.toString()) != oldValue) {
-                    Toast.makeText(getApplicationContext(), getString(R.string.toast_prefs_restart), Toast.LENGTH_LONG).show();
-                }
-                return true;
+            sendBroadcastPrefChanged();
+        } else if (key.equals("pref_gps_updates")) {
+            int newValue = Integer.parseInt(preferences.getString
+                    ("pref_gps_updates", "0"));
+            if (newValue < 2) { // user has been warned
+                Toast.makeText(getApplicationContext(), getString(R.string.pref_gps_updates_too_low), Toast.LENGTH_SHORT).show();
+            } else if (FgpstService.isRunning) {
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_prefs_restart), Toast.LENGTH_LONG).show();
             }
-        });
+        }
     }
 
     private void sendBroadcastPrefChanged() {
